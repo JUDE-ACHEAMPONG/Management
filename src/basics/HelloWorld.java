9170bb25a22044b26bddba1e09c4343d11c43c34
JUDE-ACHEAMPONG <37490872+JUDE-ACHEAMPONG@users.noreply.github.com>
2018-03-17 22:53:29 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,7 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("I am add a new code");
+		System.out.println("I am adding a new code");
 	}
 	
 	
